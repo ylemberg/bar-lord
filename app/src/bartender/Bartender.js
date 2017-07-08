5d984af44a561396da8887444b79a377dd6a3ea1
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-08 12:54:44 -0700

    [fix] Correct IP address for socket connection

--- app/src/bartender/Bartender.js
+++ app/src/bartender/Bartender.js
@@ -31,7 +31,7 @@ class Bartender extends Component {
   }
 
   componentDidMount() {
-    const externalIp = '54.197.9.47'
+    const externalIp = '34.203.247.245'
     const webSocketUri = `ws://${externalIp}:3000`
     const socket = io(webSocketUri)
     console.log('socket is', socket)
